cb2d825a8a0de6b446aa0f42fb96b969f820cab8
Victor Lascares <vmlascares@gmail.com>
2022-04-24 22:40:52 -0500

    Agregando variable a utilizar y definiendo funcion principal para mandar a llamar los Listeners

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Variables
+const carrito = document.querySelector('#carrito')
+const contenedorCarrito = document.querySelector('#lista-carrito tbody')
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
+const listaCursos = document.querySelector('#lista-cursos')
+
+cargarEventListener()
+function cargarEventListener() {
+  // Cuando se agrega un curso presionando "Agregar al Carrito"
+  listaCursos.addEventListener('click', agregarCurso)
+}
+
+// Funciones
+function agregarCurso( e ) {
+  e.preventDefault()
+  if ( e.target.classList.contains('agregar-carrito') ) {
+    console.log('Agregando al carrito');
+  }
+}
